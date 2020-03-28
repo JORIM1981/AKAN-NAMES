169a43f00f2dfbab7b15413f4ef63a59832c7a1e
JORIM1981 <okongo.midumbi.opondo@gmail.com>
2020-03-28 21:45:41 +0300

    add initialize days of the week array for both female and male names

--- akan.js
+++ akan.js
@@ -6,3 +6,25 @@ function MakeArray(n) {
     this[i]=0
   }
 }
+
+// Initialize Days of the Week Array for MALE NAMES
+
+days=new MakeArray(7);
+days[0]="Kwame"
+days[1]="Kwasi"
+days[2]="Kwadwo"
+days[3]="Kwabena"
+days[4]="Kwaku"
+days[5]="Yaw"
+days[6]="Kofi"
+
+// Initialize Days of the Week Array for FEMALE NAMES
+
+days=new MakeArray(7);
+days[0]="Ama"
+days[1]="Akosua"
+days[2]="Adwoa"
+days[3]="Abenaa"
+days[4]="Akua"
+days[5]="Yaa"
+days[6]="Afua"
