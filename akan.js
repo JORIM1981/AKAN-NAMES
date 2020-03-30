e7111e7b494e5ac4673f86728b723ea4011b1484
JORIM1981 <okongo.midumbi.opondo@gmail.com>
2020-03-30 15:21:35 +0300

    add initialize months array

--- akan.js
+++ akan.js
@@ -28,3 +28,19 @@ days[3]="Abenaa"
 days[4]="Akua"
 days[5]="Yaa"
 days[6]="Afua"
+
+// Initialize Months Array
+
+months=new MakeArray(12);
+months[1]= "January"
+months[2]= "February"
+months[3]= "March"
+months[4]= "April"
+months[5]= "May"
+months[6]= "June"
+months[7]= "July"
+months[8]= "August"
+months[9]= "September"
+months[10]= "October"
+months[11]= "November"
+months[12]= "December"
